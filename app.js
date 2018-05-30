e12e5fb494074ff319893ed2295cf4c7f8a1909d
rahul <rahulsingh2240@gmail.com>
2018-05-30 23:23:25 +0530

    dynamic searching

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  var encoded_address = encodeURIComponent(argv['a']);
 request({
-  url: "https://maps.googleapis.com/maps/api/geocode/json?address=rajat vihar noida&key=AIzaSyCcR9TZhtsEtEd2ACcbceWqaYvpXcUMtx0",
+  url: "https://maps.googleapis.com/maps/api/geocode/json?address="+encoded_address +"&key=AIzaSyCcR9TZhtsEtEd2ACcbceWqaYvpXcUMtx0",
   json: true
 }, (error, response, body)=>{
+  console.log("Address: " + body.results[0].formatted_address);
   console.log("Latitude: " + body.results[0].geometry.location.lat);
   console.log("Longitude: " + body.results[0].geometry.location.lng);
 });
