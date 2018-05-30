8ca489d042dfd0089bfc477ede89ac6becbb601e
rahul <rahulsingh2240@gmail.com>
2018-05-30 23:02:55 +0530

    Printing latitude and longitude

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
 request({
-  url: "https://maps.googleapis.com/maps/api/geocode/json?address=noida",
+  url: "https://maps.googleapis.com/maps/api/geocode/json?address=rajat vihar noida&key=AIzaSyCcR9TZhtsEtEd2ACcbceWqaYvpXcUMtx0",
   json: true
 }, (error, response, body)=>{
-  console.log(body);
+  console.log("Latitude: " + body.results[0].geometry.location.lat);
+  console.log("Longitude: " + body.results[0].geometry.location.lng);
 });
